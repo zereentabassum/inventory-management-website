2808dcff35cfb8b4a785dafc18733eac58de52ac
zereentabassum <islamms262@gmail.com>
2022-04-29 02:35:05 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Components/Blogs/Blogs';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+       <Route path='/signup' element={<Signup></Signup>}></Route>
+       <Route path='*' element={<NotFound></NotFound>}></Route>
+     </Routes>
     </div>
   );
 }
