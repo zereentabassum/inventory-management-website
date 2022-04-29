51ce6967f1dfaafe8d187709f27d4f5129f6dfb6
zereentabassum <islamms262@gmail.com>
2022-04-29 17:24:45 +0600

    navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { Container, Nav, NavDropdown } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Container className=''>
+  <Navbar.Brand ><i className='fs-2'>Marvella</i></Navbar.Brand>
+
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+    <Nav className="px-5 mx-5">
+    <Navbar.Brand className='px-4' as={Link}to="/home">Home</Navbar.Brand>
+    <Navbar.Brand className='px-4' as={Link}to="/blogs">Blogs</Navbar.Brand>
+    <Navbar.Brand className='px-4' as={Link}to="/login">Login</Navbar.Brand>
+    <Navbar.Brand className='px-4' as={Link}to="/signup">Sign up</Navbar.Brand>
+
     </Nav>
   </Navbar.Collapse>
   </Container>
