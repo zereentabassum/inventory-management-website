025f56a73849ca6eda117789b1d757cd112c6e5d
zereentabassum <islamms262@gmail.com>
2022-04-29 19:53:46 +0600

    footer added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+      <div className='bg-info'>
+      <div className=' '>
+          <div className="py-5 px-1  text-start footer">
+
+          <div  className='px-3'>
+                <h5>Contact us</h5>
+                <p>Address: 1234 Street, SK</p>
+                <p>Email: Marvella34@gmail.com</p>
+            </div>
+            <div  className=''>
+                <h5>About us</h5>
+                <p>Community</p>
+                <p>Support</p>
+                <p>Articles</p>
+                <p>Guides</p>
+            </div>
+            <div  className=''>
+                <h5>Learn More</h5>
+                <p>Terms and Conditions</p>
+                <p>Help</p>
+                <p>Latest News</p>
+                <p>FAQ</p>
+            </div>
+          </div>
+          </div>
+          <div className='pb-4'>
+          <p>Copyright © 2022. All rights reserved</p>
+          </div>
+          </div>  
+    );
+};
+
+export default Footer;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <h1>HOme</h1>
             <hr />
+            <Footer></Footer>
         </div>
     );
 };
