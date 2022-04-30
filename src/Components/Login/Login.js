ce48153dfef0a017506bf63b4cef523864496412
zereentabassum <islamms262@gmail.com>
2022-04-30 20:31:12 +0600

    login and signup form created

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import GoogleSignIn from '../GoogleSignIn/GoogleSignIn';
 
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
+            <h1 className='pt-4 pb-3'>Login</h1>                    <Form className='form'>
+                <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
+                <Form.Label  >Email address:</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+                <Form.Label className='text-start'>Password:</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="danger px-5 mb-3" type="submit">
+                Login
+                </Button>
+                <br />
+    <Button className='text-decoration-none'  variant='link'>Forgot Password? Reset</Button>
+    <div className='d-flex align-items-center'>
+        <div style={{height:'1px'}} className='bg-danger w-50'></div>
+        <h5 className='mt-2 px-2'>Or</h5>
+        <div style={{height:'1px'}} className='bg-danger w-50'></div>
+    </div>
+  
+    <GoogleSignIn></GoogleSignIn>
+    <p>Don't have an account? <Link className='text-decoration-none' to='/signup'>Sign up</Link></p>
+   
+                
+</Form>
         </div>
     );
 };
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Signup.css';
 
 const Signup = () => {
     return (
         <div>
-            <h1>Sign up</h1>
-            <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+            <h1 className='pt-3 pb-2'>Sign up</h1>
+            <Form className='form'>
+            <Form.Group className='text-start pb-3'>
+            <Form.Label >Name:</Form.Label>
+            <Form.Control type="text" 
+            placeholder="Your name" required/>
+            </Form.Group>
+            <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
+              <Form.Label  >Email address:</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
+            <Form.Group className="mb-3 text-start" controlId="formBasicPassword">
+              <Form.Label className='text-start'>Password:</Form.Label>
+              <Form.Control type="password" placeholder="Password" required/>
+              <Form.Text className='text-success fs-6' id="passwordHelpBlock" >
+              Password should be at least 6 characters or more. 
+              </Form.Text>
+            </Form.Group>
+            <Button variant="danger px-5 mb-3" type="submit">
+              Signup
+            </Button>
+            <p>Already have an account? <Link className='text-decoration-none' to='/login'>Login</Link></p>
+            
 </Form>
         </div>
     );
